3407cd1aa09f121b5de591f30b15e572fc1a446c
Alex Vallone <vallone@chapman.edu>
2021-01-13 12:44:22 -0800

    2nd Commit

---
+++ Fish Hunt/Assets/Resources/Script/GameSetupController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using System.IO;
+
+public class GameSetupController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreatePlayer();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void CreatePlayer() {
+        Debug.Log("Creating Player");
+        PhotonNetwork.Instantiate(Path.Combine("Prefabs", "PlayerPrefab"), Vector3.zero, Quaternion.identity);
+    }
+}
