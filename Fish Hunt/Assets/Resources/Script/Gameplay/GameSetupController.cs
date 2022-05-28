30d0d83109f04b1697f17cd9ffe41d91c10e4500
amv146 <vallone@chapman.edu>
2022-05-28 13:34:19 -0700

    UI Fixes

---
+++ Fish Hunt/Assets/Resources/Script/Gameplay/GameSetupController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using System.IO;
+
+public class GameSetupController : MonoBehaviour
+{
+    public static GameSetupController instance;
+
+    public static GameSetupController Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    public GameObject ScoreboardPrefab;
+    public GameObject ShotsboardPrefab;
+    public GameObject ScorePrefab;
+
+    private void Awake() {
+        instance = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreatePlayer();
+    }
+
+    private void CreatePlayer() {
+        Debug.Log("Creating Player");
+        GameObject crosshair = PhotonNetwork.Instantiate(Path.Combine("Prefabs", "Game", "Crosshair"), Vector3.zero, Quaternion.identity);
+        GameObject scoreboard = PhotonNetwork.Instantiate(Path.Combine("Prefabs", "UI", "Scoreboard"), ScoreboardPrefab.transform.position, Quaternion.identity);
+        GameObject shotsboard = PhotonNetwork.Instantiate(Path.Combine("Prefabs", "UI", "Shotsboard"), ShotsboardPrefab.transform.position, Quaternion.identity);
+        GameObject score = PhotonNetwork.Instantiate(Path.Combine("Prefabs", "UI", "Score"), ScorePrefab.transform.position, Quaternion.identity);
+        Photon.Realtime.Player photonPlayer = PhotonNetwork.LocalPlayer;
+        object[] viewIDs = new object[]
+        {
+            crosshair.GetPhotonView().ViewID, scoreboard.GetPhotonView().ViewID, shotsboard.GetPhotonView().ViewID, score.GetPhotonView().ViewID, photonPlayer.UserId
+        };
+
+        PhotonNetwork.Instantiate(Path.Combine("Prefabs", "UI", "Player"), Vector3.zero, Quaternion.identity, 0, viewIDs);
+    }
+}
---
+++ Fish Hunt/Assets/Resources/Script/Networking/LobbyController.cs
@@ -0,0 +1,29 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LobbyController : MonoBehaviourPunCallbacks
+{
+    private const int roomSize = 2;
+    
+    public override void OnJoinRandomFailed(short returnCode, string message) {
+        Debug.Log("Failed to join a room");
+        CreateRoom();
+    }
+
+    public void CreateRoom() {
+        Debug.Log("Creating Room Now.");
+        int randomRoomNumber = Random.Range(0, 10000);
+        RoomOptions roomOps = new RoomOptions() { IsVisible = true, MaxPlayers = (byte)roomSize };
+        PhotonNetwork.CreateRoom("Room" + randomRoomNumber, roomOps);
+        Debug.Log(randomRoomNumber);
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message) {
+        Debug.Log("Failed to create room... trying again");
+        CreateRoom();
+    }
+
+}
